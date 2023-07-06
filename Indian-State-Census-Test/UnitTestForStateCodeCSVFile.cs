54051225b69bc70509950c717ebab27218a79591
Vedant <vedantnp200@gmail.com>
2023-07-06 15:45:16 +0530

    [Vedant]-Test the number of records are return

---
+++ Indian-State-Census-Test/UnitTestForStateCodeCSVFile.cs
@@ -0,0 +1,22 @@
+﻿using Indian_State_Census_Problem;
+
+namespace Indian_State_Census_Test
+{
+    [TestClass]
+    public class UnitTestForStateCodeCSVFile
+    {
+        [TestMethod]
+        public void TestCase1()
+        {
+            string FilePath = @"D:\Courses\C#\Indian State Census Project\Indian-State-Census-Problem\StateCode.csv";
+
+            StateCodeAnalyser analyser = new StateCodeAnalyser();
+
+            int actual = analyser.ReadAndCountStateCodeData(FilePath);
+
+            int expected = 37;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
